5cadc9b4e8c7983ffa71ce3405bec9fbb69f61da
revathiest <revathiest@gmail.com>
2025-06-08 09:25:25 -0500

    Updating .gitignore and config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.PFC_CONFIG = {
-  apiBase: 'https://api.pyrofreelancercorps.com',
-  discordClientId: '819004565869035531',
-  redirectUri: 'https://pyrofreelancercorps.com/'
+  apiBase: 'http://localhost:8003',
+  discordClientId: '1013095667201228810',
+  redirectUri: 'http://localhost:5500/'
 };
